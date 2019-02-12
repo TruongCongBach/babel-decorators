7b53dc1e558febaf426ed0633ed99cbf63a44aa1
TruongCongBach <truongcongbach1996@gmail.com>
2019-02-12 09:16:02 +0700

    Happy New Year 2019

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>Happy New Year 2019</code> and save to reload.
           </p>
           <a
             className="App-link"
